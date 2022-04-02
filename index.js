beb7555173799ffcd76e2c7d03387fb1abe60085
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2022-04-02 17:14:27 +0700

    adding create and read function on index

--- index.js
+++ index.js
@@ -2,6 +2,44 @@ const express = require('express')
 const app = express()
 const pool = require('./database/db')
 
+app.use(express.json()) 
+
+
+
+//ROUTES
+// get all todos
+app.get('/todos', async (req, res) => {
+    try {
+        const allTodos = await pool.query('SELECT * FROM todo')
+        res.json(allTodos.rows)
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+//get a todo
+
+//create a todo
+app.post('/todos/add', async (req, res) => {
+    try {
+        const { description } = req.body;
+
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES ($1) RETURNING *", 
+        [description]
+        );
+
+        res.json(newTodo.rows[0]);
+    } catch (error) {
+        console.error(err.message)
+    }
+})
+
+//update a todo
+
+//delete a todo
+
+
+
 app.listen(5000, () => {
     console.log("Server is running on port 5000")
 })
